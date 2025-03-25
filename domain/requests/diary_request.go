4e68ea8deba4927c3b23d6cb642f9dee470c0084
kritpi <kritpiruch@gmail.com>
2025-03-25 14:01:44 +0700

    Add multiple images to diary

--- domain/requests/diary_request.go
+++ domain/requests/diary_request.go
@@ -1,15 +1,20 @@
 package requests
 
 type CreateDiaryRequest struct {
-    Date        string   `json:"date" `
-    Mood        string   `json:"mood"`
-    Emotions    []string `json:"emotions"`
-    Description string   `json:"description"`
-    UserID      string   `json:"user_id"`
+	Date        string   `json:"date" `
+	Mood        string   `json:"mood"`
+	Emotions    []string `json:"emotions"`
+	Description string   `json:"description"`
+	UserID      string   `json:"user_id"`
+	Images      []string `json:"images"`
 }
 
-type UpdateDiaryRequest struct{
-    Mood        string   `json:"mood"`
-    Emotions    []string `json:"emotions"`
-    Description string   `json:"description"`
+type UpdateDiaryRequest struct {
+	Date        string   `json:"date" `
+	UserID      string   `json:"user_id"`
+	Mood        string   `json:"mood"`
+	Emotions    []string `json:"emotions"`
+	Description string   `json:"description"`
+	Images      []string `json:"images"`
+	ImagesURL   []string `json:"images_url"`
 }
