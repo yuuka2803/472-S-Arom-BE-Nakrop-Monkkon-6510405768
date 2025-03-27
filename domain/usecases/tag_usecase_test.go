81a2c17c14c37858e8d57e74b350f5368affd6b8
maycozygirl <may11682.o@gmail.com>
2025-03-27 15:00:31 +0700

    edit: TestGetById

--- domain/usecases/tag_usecase_test.go
+++ domain/usecases/tag_usecase_test.go
@@ -10,17 +10,17 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/kritpi/arom-web-services/domain/usecases"
-	"github.com/kritpi/arom-web-services/domain/usecases/test/mock_repos"
+	mockrepos "github.com/kritpi/arom-web-services/domain/usecases/test/mock_repos"
 )
 
-func TestGetByID(t *testing.T){
-	MockTagRepo := &mockrepos.MockTagRepo{}
+func TestGetByID(t *testing.T) {
 	ctx := context.Background()
 
-	t.Run("Success", func(t *testing.T){
-		MockTagRepo.On("GetByID", ctx, "1").Return(&models.Tag{}, nil)
+	t.Run("Success", func(t *testing.T) {
+		mockTagRepo := &mockrepos.MockTagRepo{}
+		mockTagRepo.On("GetByID", ctx, "1").Return(&models.Tag{}, nil)
 
-		tagService := usecases.ProvideTagService(MockTagRepo, &configs.Config{})
+		tagService := usecases.ProvideTagService(mockTagRepo, &configs.Config{})
 		tag, err := tagService.GetByIDTag(ctx, "1")
 
 		if err != nil {
@@ -30,15 +30,33 @@ func TestGetByID(t *testing.T){
 		if reflect.TypeOf(tag) != reflect.TypeOf(&models.Tag{}) {
 			t.Errorf("Expected type %v but got %v", reflect.TypeOf(&models.Tag{}), reflect.TypeOf(tag))
 		}
-		
-		assert.Equal(t, tag, &models.Tag{})
-		MockTagRepo.AssertExpectations(t)
+
+		mockTagRepo.AssertExpectations(t)
 	})
 
-	t.Run("Error", func(t *testing.T){
-		MockTagRepo.On("GetByID", ctx, "1").Return(nil, assert.AnError)
+	t.Run("Not Found", func(t *testing.T) {
+		mockTagRepo := &mockrepos.MockTagRepo{}
+		mockTagRepo.On("GetByID", ctx, "1").Return(nil, nil)
 
-		tagService := usecases.ProvideTagService(MockTagRepo, &configs.Config{})
+		tagService := usecases.ProvideTagService(mockTagRepo, &configs.Config{})
+		tag, err := tagService.GetByIDTag(ctx, "1")
+
+		if err != nil {
+			t.Errorf("Error was not expected: %v", err)
+		}
+
+		if tag != nil {
+			t.Errorf("Expected nil but got %v", tag)
+		}
+
+		mockTagRepo.AssertExpectations(t)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		mockTagRepo := &mockrepos.MockTagRepo{}
+		mockTagRepo.On("GetByID", ctx, "1").Return(nil, assert.AnError)
+
+		tagService := usecases.ProvideTagService(mockTagRepo, &configs.Config{})
 		tag, err := tagService.GetByIDTag(ctx, "1")
 
 		if err == nil {
@@ -48,77 +66,7 @@ func TestGetByID(t *testing.T){
 		if tag != nil {
 			t.Errorf("Expected nil but got %v", tag)
 		}
-		MockTagRepo.AssertExpectations(t)
-	})
 
-}
-
-func TestGetByUserID(t *testing.T){
-	MockTagRepo := &mockrepos.MockTagRepo{}
-	ctx := context.Background()
-
-	t.Run("Success", func(t *testing.T){
-		MockTagRepo.On("GetByUserID", ctx, "1").Return([]*models.Tag{}, nil)
-
-		tagService := usecases.ProvideTagService(MockTagRepo, &configs.Config{})
-		tags, err := tagService.GetByUserIDTag(ctx, "1")
-
-		if err != nil {
-			t.Errorf("Error was not expected: %v", err)
-		}
-
-		if reflect.TypeOf(tags) != reflect.TypeOf([]*models.Tag{}) {
-			t.Errorf("Expected type %v but got %v", reflect.TypeOf([]*models.Tag{}), reflect.TypeOf(tags))
-		}
-		
-		assert.Equal(t, tags, []*models.Tag{})
-		MockTagRepo.AssertExpectations(t)
-	})
-
-	t.Run("Error", func(t *testing.T){
-		MockTagRepo.On("GetByUserID", ctx, "1").Return(nil, assert.AnError)
-
-		tagService := usecases.ProvideTagService(MockTagRepo, &configs.Config{})
-		tags, err := tagService.GetByUserIDTag(ctx, "1")
-
-		if err == nil {
-			t.Errorf("Error was expected but got nil")
-		}
-
-		if tags != nil {
-			t.Errorf("Expected nil but got %v", tags)
-		}
-		MockTagRepo.AssertExpectations(t)
-	})
-}
-
-func TestDelete(t *testing.T){
-	MockTagRepo := &mockrepos.MockTagRepo{}
-	ctx := context.Background()
-
-	t.Run("Success", func(t *testing.T){
-		MockTagRepo.On("Delete", ctx, "1").Return(nil)
-
-		tagService := usecases.ProvideTagService(MockTagRepo, &configs.Config{})
-		err := tagService.DeleteTag(ctx, "1")
-
-		if err != nil {
-			t.Errorf("Error was not expected: %v", err)
-		}
-
-		MockTagRepo.AssertExpectations(t)
-	})
-
-	t.Run("Error", func(t *testing.T){
-		MockTagRepo.On("Delete", ctx, "1").Return(assert.AnError)
-
-		tagService := usecases.ProvideTagService(MockTagRepo, &configs.Config{})
-		err := tagService.DeleteTag(ctx, "1")
-
-		if err == nil {
-			t.Errorf("Error was expected but got nil")
-		}
-
-		MockTagRepo.AssertExpectations(t)
+		mockTagRepo.AssertExpectations(t)
 	})
 }
